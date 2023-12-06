6ac3751d443e04dfcfdc8e7eec4a4a2f1e53f165
GitJason33 <121455569+GitJason33@users.noreply.github.com>
2023-12-06 17:52:16 +0200

    fixed an api bug in client

--- client/config.js
+++ client/config.js
@@ -1,5 +1,5 @@
 export default {
   "api-key": "kazablanca",
   "login-cookie": "TodoLogin",
-  "api-domain": "https://api-todolistjason33.onrender.com",
+  "api-domain": "https://api-todolistjason33.onrender.com/api",
 }
